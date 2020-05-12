2326e2d897e547647ea932726d816e38ce3c186e
Troy Pocklington <tdashroy@gmail.com>
2020-05-11 22:57:31 -0700

    removed outer if => logic fun

--- Brainfuck/Program.cs
+++ Brainfuck/Program.cs
@@ -6,26 +6,22 @@
         {
             var a=new char[99];
             var s=new int[9];
-            var f=0<0;
-            for(int i=0,d=0,t=0;i<p.Length;++i)
+            var f=1>0;
+            try
             {
-                var c=p[i];
-                if(f)
+                for(int i=0,d=0,t=0;;)
                 {
-                    if(c=='[')s[t++]=i;
-                    f=c==']'?s[--t]>=0:f;
-                }
-                else
-                {
-                    if(c=='>')++d;
-                    if(c=='<')--d;
-                    if(c=='+')++a[d];
-                    if(c=='-')--a[d];
-                    if(c=='.')System.Console.Write(a[d]);
-                    if(c=='[')s[t++]=(f=a[d]==0)?-1:i;
-                    if(c==']')i=s[--t]-1;
+                    var c=p[i++];
+                    if(f&c=='>')++d;
+                    if(f&c=='<')--d;
+                    if(f&c=='+')++a[d];
+                    if(f&c=='-')--a[d];
+                    if(f&c=='.')System.Console.Write(a[d]);
+                    if(c=='[')s[t++]=f?(f=a[d]!=0)?i:-1:i;
+                    if(c==']')i=(f&(f|=s[--t]<0))?s[t]-1:i;
                 }
             }
+            catch{}
         }
     }
 }
