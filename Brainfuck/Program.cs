83b75fe89458dfd63d96b17511c644ed76cdcd79
Troy Pocklington <tdashroy@gmail.com>
2020-05-12 01:07:09 -0700

    moving ++i to the end of the loop

--- Brainfuck/Program.cs
+++ Brainfuck/Program.cs
@@ -8,14 +8,14 @@
             {
                 for(dynamic a=new char[99],s=new int[9],f=1>0,i=0,d=0,t=0;;)
                 {
-                    var c=p[i++];
+                    var c=p[i];
                     if(f&c=='>')++d;
                     if(f&c=='<')--d;
                     if(f&c=='+')++a[d];
                     if(f&c=='-')--a[d];
                     if(f&c=='.')System.Console.Write(a[d]);
                     if(c=='[')s[t++]=f?(f=a[d]!=0)?i:-1:i;
-                    i=(c==']'&&f&(f|=s[--t]<0))?s[t]-1:i;
+                    i=(c==']'&&f&(f|=s[--t]<0))?s[t]:++i;
                 }
             }
             catch{}
