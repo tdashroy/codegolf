e7495937ff1fd3e647e02ef93015728d7d5866fb
Troy Pocklington <tdashroy@gmail.com>
2020-05-11 19:52:52 -0700

    removed collections

--- Brainfuck/Program.cs
+++ Brainfuck/Program.cs
@@ -1,48 +1,45 @@
-﻿namespace System.Collections.Generic
+﻿class C
 {
-    class C
+    static void Main(string[] z)
     {
-        static void Main(string[] z)
+        foreach(var p in z)
         {
-            foreach(var p in z)
+            var a=new char[99];
+            var s=new int[9];
+            var f=0<0;
+            for(int i=0,d=0,t=0;i<p.Length;++i)
             {
-                var a=new char[99];
-                var s=new Stack<int>();
-                var f=0<0;
-                for(int i=0,d=0;i<p.Length;++i)
+                if(f)
                 {
-                    if(f)
+                    if(p[i]=='[')s[t++]=i;
+                    f=p[i]==']'?s[--t]>=0:f;
+                }
+                else
+                {
+                    switch(p[i])
                     {
-                        if(p[i]=='[')s.Push(i);
-                        f=p[i]==']'?s.Pop()>=0:f;
-                    }
-                    else
-                    {
-                        switch(p[i])
-                        {
-                            case'>':
-                                ++d;
-                                break;
-                            case'<':
-                                --d;
-                                break;
-                            case'+':
-                                ++a[d];
-                                break;
-                            case'-':
-                                --a[d];
-                                break;
-                            case'.':
-                                Console.Write(a[d]);
-                                break;
-                            case'[':
-                                f=a[d]==0;
-                                s.Push(f?-1:i);
-                                break;
-                            case']':
-                                i=s.Pop()-1;
-                                break;
-                        }
+                        case'>':
+                            ++d;
+                            break;
+                        case'<':
+                            --d;
+                            break;
+                        case'+':
+                            ++a[d];
+                            break;
+                        case'-':
+                            --a[d];
+                            break;
+                        case'.':
+                            System.Console.Write(a[d]);
+                            break;
+                        case'[':
+                            f=a[d]==0;
+                            s[t++]=f?-1:i;
+                            break;
+                        case']':
+                            i=s[--t]-1;
+                            break;
                     }
                 }
             }
