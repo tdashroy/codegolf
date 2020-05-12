2db267872f00f4e2c66f52973752006851c5cd0d
Troy Pocklington <tdashroy@gmail.com>
2020-05-11 20:13:07 -0700

    switch -> if

--- Brainfuck/Program.cs
+++ Brainfuck/Program.cs
@@ -9,38 +9,21 @@
             var f=0<0;
             for(int i=0,d=0,t=0;i<p.Length;++i)
             {
+                var c=p[i];
                 if(f)
                 {
-                    if(p[i]=='[')s[t++]=i;
-                    f=p[i]==']'?s[--t]>=0:f;
+                    if(c=='[')s[t++]=i;
+                    f=c==']'?s[--t]>=0:f;
                 }
                 else
                 {
-                    switch(p[i])
-                    {
-                        case'>':
-                            ++d;
-                            break;
-                        case'<':
-                            --d;
-                            break;
-                        case'+':
-                            ++a[d];
-                            break;
-                        case'-':
-                            --a[d];
-                            break;
-                        case'.':
-                            System.Console.Write(a[d]);
-                            break;
-                        case'[':
-                            f=a[d]==0;
-                            s[t++]=f?-1:i;
-                            break;
-                        case']':
-                            i=s[--t]-1;
-                            break;
-                    }
+                    if(c=='>')++d;
+                    if(c=='<')--d;
+                    if(c=='+')++a[d];
+                    if(c=='-')--a[d];
+                    if(c=='.')System.Console.Write(a[d]);
+                    if(c=='[')s[t++]=(f=a[d]==0)?-1:i;
+                    if(c==']')i=s[--t]-1;
                 }
             }
         }
