f8720b90730346974b99e55a2a5915b8a9c3b167
Troy Pocklington <tdashroy@gmail.com>
2020-05-12 01:15:34 -0700

    removing fizzbuzz since i didn't come up with it on my own

--- FizzBuzz/Program.cs
+++
@@ -1,7 +0,0 @@
-﻿class C 
-{ 
-    static void Main() 
-    { 
-        for (var i = 0; i++ < 100; System.Console.Write($"{(i % 3 * i % 5 > 0 ? i : 0):#}{i % 3:;;Fizz}{i % 5:;;Buzz}\n")) ; 
-    }
-}
