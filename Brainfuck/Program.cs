fb3a2e61d7299e8fefbf7ae97a8ac5a2883f9ef5
Troy Pocklington <tdashroy@gmail.com>
2020-05-11 19:31:03 -0700

    initial commit

---
+++ Brainfuck/Program.cs
@@ -0,0 +1,51 @@
+﻿namespace System.Collections.Generic
+{
+    class C
+    {
+        static void Main(string[] z)
+        {
+            foreach(var p in z)
+            {
+                var a=new char[99];
+                var s=new Stack<int>();
+                var f=0<0;
+                for(int i=0,d=0;i<p.Length;++i)
+                {
+                    if(f)
+                    {
+                        if(p[i]=='[')s.Push(i);
+                        f=p[i]==']'?s.Pop()>=0:f;
+                    }
+                    else
+                    {
+                        switch(p[i])
+                        {
+                            case'>':
+                                ++d;
+                                break;
+                            case'<':
+                                --d;
+                                break;
+                            case'+':
+                                ++a[d];
+                                break;
+                            case'-':
+                                --a[d];
+                                break;
+                            case'.':
+                                Console.Write(a[d]);
+                                break;
+                            case'[':
+                                f=a[d]==0;
+                                s.Push(f?-1:i);
+                                break;
+                            case']':
+                                i=s.Pop()-1;
+                                break;
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ FizzBuzz/Program.cs
@@ -0,0 +1,7 @@
+﻿class C 
+{ 
+    static void Main() 
+    { 
+        for (var i = 0; i++ < 100; System.Console.Write($"{(i % 3 * i % 5 > 0 ? i : 0):#}{i % 3:;;Fizz}{i % 5:;;Buzz}\n")) ; 
+    }
+}
