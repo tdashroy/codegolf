b189dcc7145b8271009b905b5a11d2075a08af64
Troy Pocklington <tdashroy@gmail.com>
2020-05-12 18:54:54 -0700

    using character codes instead of characters

--- Brainfuck/Program.cs
+++ Brainfuck/Program.cs
@@ -10,14 +10,14 @@ class P
                 for(dynamic a=new int[30000],s=new int[9],f=1>0,i=0,d=0,t=0;;)
                 {
                     var c=p[i];
-                    if(f&c=='>')++d;
-                    if(f&c=='<')--d;
-                    if(f&c=='+')++a[d];
-                    if(f&c=='-')--a[d];
-                    if(f&c=='.')C.Write((char)a[d]);
-                    if(f&c==',')a[d]=C.Read();
-                    if(c=='[')s[t++]=f?(f=a[d]!=0)?i:-1:i;
-                    i=(c==']'&&f&(f|=s[--t]<0))?s[t]:++i;
+                    if(f&c==62)++d;
+                    if(f&c==60)--d;
+                    if(f&c==43)++a[d];
+                    if(f&c==45)--a[d];
+                    if(f&c==46)C.Write((char)a[d]);
+                    if(f&c==44)a[d]=C.Read();
+                    if(c==91)s[t++]=f?(f=a[d]!=0)?i:-1:i;
+                    i=(c==93&&f&(f|=s[--t]<0))?s[t]:++i;
                 }
             }
             catch{}
