a1f3e7770f0bc46b69585e68d06a2f709f0cdb74
Troy Pocklington <tdashroy@gmail.com>
2020-07-23 21:12:18 -0700

    combining +/- and >/<

--- code-golf.io/Brainfuck/Brainfuck.io.cs
+++ code-golf.io/Brainfuck/Brainfuck.io.cs
@@ -9,10 +9,8 @@
                 for(dynamic c,a=new char[99],s=new int[9],f=1>0,i=0,d=0,t=0;;)
                 {
                     c=p[i];
-                    if(f&c==62)++d;
-                    if(f&c==60)--d;
-                    if(f&c==43)++a[d];
-                    if(f&c==45)--a[d];
+                    d+=(c-61)*(f&c>59&c<63?1:0);
+                    a[d]+=(44-c)*(f&c>42&c<46?1:0);
                     if(f&c==46)System.Console.Write(a[d]);
                     if(c==91)s[t++]=f?(f=a[d]!=0)?i:-1:i;
                     i=(c==93&&f&(f|=s[--t]<0))?s[t]:++i;
