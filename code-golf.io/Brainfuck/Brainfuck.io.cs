d189f57f9e2e67c8b53d4311fb362fe4296cc32b
Troy Pocklington <tdashroy@gmail.com>
2020-08-10 01:43:17 -0700

    combining +/- and >/< into a single line and removing unecessary parens in the i=...

--- code-golf.io/Brainfuck/Brainfuck.io.cs
+++ code-golf.io/Brainfuck/Brainfuck.io.cs
@@ -6,11 +6,10 @@
         {
             try
             {
-                for(dynamic c,a=new char[99],s=new int[9],f=1>0,i=0,d=0,t=0;;i=(c==51&&f&(f|=s[--t]<0))?s[t]:++i)
+                for(dynamic c,a=new char[99],s=new int[9],f=1>0,i=0,d=0,t=0;;i=c==51&&f&(f|=s[--t]<0)?s[t]:++i)
                 {
                     c=p[i]-42;
-                    d+=(c-19)*(f&c>17&c<21?1:0);
-                    a[d]+=(2-c)*(f&c>0&c<4?1:0);
+                    a[d+=(c-19)*(f&c>17&c<21?1:0)]+=(2-c)*(f&c>0&c<4?1:0);
                     if(f&c==4)System.Console.Write(a[d]);
                     if(c==49)s[t++]=f?(f=a[d]!=0)?i:-1:i;
                 }
--- codegolf.stackexchange/Brainfuck.stackexchange/Brainfuck.stackexchange.cs
+++ codegolf.stackexchange/Brainfuck.stackexchange/Brainfuck.stackexchange.cs
@@ -5,11 +5,10 @@ class P
     {
         try
         {
-            for(dynamic c,p=z[0],a=new int[30000],s=new int[9],f=1>0,i=0,d=0,t=0;;i=(c==51&&f&(f|=s[--t]<0))?s[t]:++i)
+            for(dynamic c,p=z[0],a=new int[30000],s=new int[9],f=1>0,i=0,d=0,t=0;;i=c==51&&f&(f|=s[--t]<0)?s[t]:++i)
             {
                 c=p[i]-42;
-                d+=(c-19)*(f&c>17&c<21?1:0);
-                a[d]+=(2-c)*(f&c>0&c<4?1:0);
+                a[d+=(c-19)*(f&c>17&c<21?1:0)]+=(2-c)*(f&c>0&c<4?1:0);
                 if(f&c==4)C.Write((char)a[d]);
                 if(f&c==2)a[d]=C.Read();
                 if(c==49)s[t++]=f?(f=a[d]!=0)?i:-1:i;
