0eb4fe052a05dbbfdf81c89b0fc1305141ecdfac
Troy Pocklington <tdashroy@gmail.com>
2020-08-09 21:25:19 -0700

    making number literals smaller by subtracting from c and moving i= into the loop iterator section

--- codegolf.stackexchange/Brainfuck.stackexchange/Brainfuck.stackexchange.cs
+++ codegolf.stackexchange/Brainfuck.stackexchange/Brainfuck.stackexchange.cs
@@ -5,15 +5,14 @@ class P
     {
         try
         {
-            for(dynamic c,p=z[0],a=new int[30000],s=new int[9],f=1>0,i=0,d=0,t=0;;)
+            for(dynamic c,p=z[0],a=new int[30000],s=new int[9],f=1>0,i=0,d=0,t=0;;i=(c==51&&f&(f|=s[--t]<0))?s[t]:++i)
             {
-                c=p[i];
-                d+=(c-61)*(f&c>59&c<63?1:0);
-                a[d]+=(44-c)*(f&c>42&c<46?1:0);
-                if(f&c==46)C.Write((char)a[d]);
-                if(f&c==44)a[d]=C.Read();
-                if(c==91)s[t++]=f?(f=a[d]!=0)?i:-1:i;
-                i=(c==93&&f&(f|=s[--t]<0))?s[t]:++i;
+                c=p[i]-42;
+                d+=(c-19)*(f&c>17&c<21?1:0);
+                a[d]+=(2-c)*(f&c>0&c<4?1:0);
+                if(f&c==4)C.Write((char)a[d]);
+                if(f&c==2)a[d]=C.Read();
+                if(c==49)s[t++]=f?(f=a[d]!=0)?i:-1:i;
             }
         }
         catch{}
